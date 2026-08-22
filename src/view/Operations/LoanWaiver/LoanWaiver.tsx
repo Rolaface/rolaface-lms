2d5b9e9b961093d980eee77fcbdb012084dec191
rohit04rawat <rohit.rawat@rolaface.com>
2026-08-22 12:12:10 +0530

    a

--- src/view/Operations/LoanWaiver/LoanWaiver.tsx
+++ src/view/Operations/LoanWaiver/LoanWaiver.tsx
@@ -704,7 +704,7 @@ export function LoanWaiver() {
                 size="xs"
                 radius="xl"
                 disabled={totalRows === 0}
-              />y
+              />
             </Group>
           </>
         )}
